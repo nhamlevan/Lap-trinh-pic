a3ed517ab42763d6e91cba63e78ebd0313168d82
DESKTOP-OO6AU4U\Le Nham <nhamlevan.92@gmail.com>
2016-09-27 10:06:33 +0700

    toi uu thu vien

--- lcd 16x2/lcd_4bit_lib.c
+++ lcd 16x2/lcd_4bit_lib.c
@@ -1,19 +1,26 @@
 //thu vien giao tiep lcd 4 bit
-/*void lcd_set_data(int8 dat){
+void lcd_set_data(int8 dat){
+   //byte z=dat;
    output_bit(LCD_D4,dat&0x01);
-   output_bit(LCD_D4,(dat>>1)&0x01);
-   output_bit(LCD_D4,(dat>>2)&0x01);
-   output_bit(LCD_D4,(dat>>3)&0x01);
-}*/
+   output_bit(LCD_D5,dat&0x02);
+   output_bit(LCD_D6,dat&0x04);
+   output_bit(LCD_D7,dat&0x08);
+  
+  
+  /* output_bit(LCD_D4,dat&0x01);
+   output_bit(LCD_D5,(dat>>1)&0x01);
+   output_bit(LCD_D6,(dat>>2)&0x01);
+   output_bit(LCD_D7,(dat>>3)&0x01);*/
+}
 void lcd_send_2nib(int8 nib){
-    LCD_DATA=(nib);
-   //lcd_set_data(nib>>4);
+    //LCD_DATA=(nib);
+   lcd_set_data(nib>>4);
     LCD_EN=0b1;
     delay_us(10); 
     LCD_EN=0b0;
     
-    LCD_DATA=nib<<4;
-   //lcd_set_data(nib);
+    //LCD_DATA=nib<<4;
+    lcd_set_data(nib);
     LCD_EN=0b1;
     delay_us(10); 
     LCD_EN=0b0;
